5d96d7d8026940731b51359cc2fe14cbab2c7f6a
FWREL <muhammadfarrel0@gmail.com>
2026-07-18 18:36:06 +0700

    feat(Panen API): implement GET endpoint for retrieving panen data with filtering options for Petani and Admin in the future.

--- src/app/api/panen/route.ts
+++ src/app/api/panen/route.ts
@@ -59,3 +59,50 @@ export async function POST(req: Request) {
     );
   }
 }
+
+export async function GET(req: Request) {
+  try {
+    // ambil query param dari URL (misal: ?petaniId=xxx atau ?status=pending)
+    const { searchParams } = new URL(req.url);
+    const petaniId = searchParams.get("petaniId");
+    const status = searchParams.get("status");
+
+    const queryOptions: any = {
+      orderBy: { createdAt: "desc" }, // urutin datar dari terbaru
+      include: {
+        // JOIN untuk mengambil data petani. untuk keperluan dashboard admin (kopdes)
+        petani: {
+          select: { name: true, location: true },
+        },
+      },
+    };
+
+    if (petaniId) {
+      // page petani request API untuk melihat riwayat setor dia
+      // Keperluan: untuk membuat riwayat transaksi antara petani dengan coco
+      queryOptions.where = { petaniId: petaniId };
+    } else if (status === "pending") {
+      // page admin req daftar barang yang harus di urus hari ini
+      // Keperluan: untuk sorting ke coco hasil panen mana yang harus dikerjakan (qc) hari ini
+      queryOptions.where = {
+        status: {
+          in: [PanenStatus.PENDING_PICKUP, PanenStatus.PENDING_DROPOFF],
+        },
+      };
+    }
+
+    // kalo gaada param maka kita return semua data
+    const dataPanen = await prisma.panen.findMany(queryOptions);
+
+    return NextResponse.json(
+      { message: "Berhasil mengambil data panen.", dataPanen },
+      { status: 200 },
+    );
+  } catch (err) {
+    console.error("Error in GET /api/panen:", err);
+    return NextResponse.json(
+      { message: "Terjadi error pada sistem." },
+      { status: 500 },
+    );
+  }
+}
