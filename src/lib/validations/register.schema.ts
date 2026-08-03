9b6a164d5ea790cefbbb9777b49d52506094e35e
Fwrrell <muhammadfarrel0@gmail.com>
2026-08-04 02:16:32 +0700

    fix: update user registration logic and validation for phone number starts with 08..

--- src/lib/validations/register.schema.ts
+++ src/lib/validations/register.schema.ts
@@ -2,15 +2,15 @@ import { z } from "zod";
 
 export const registerSchema = z.object({
   name: z.string().min(3, "Nama harus memiliki minimal 3 karakter."),
+  // validasi no hp indo dlu hrus 08 depannya
   phoneNumber: z
     .string()
-    .min(10, "Nomor HP minimal 10 digit")
-    .max(15, "Nomor HP maksimal 15 digit")
-    .regex(/^(\+62|62|0)8[1-9][0-9]{7,12}$/, "Format nomor HP Indonesia tidak valid."),
+    .min(10, "Nomor HP minimal 10 digit.")
+    .max(13, "Nomor HP maksimal 13 digit.")
+    .regex(/^08\d{8,11}$/, "Nomor HP harus diawali '08' dan berisi 10-13 digit angka."),
   pin: z
     .string()
     .length(6, "PIN harus 6 digit.")
     .regex(/^\d+$/, "PIN hanya boleh berisi angka."),
-  // Ganti location dengan kopdesId, dan pastikan itu UUID jika ada
-  kopdesId: z.string().uuid("ID Kopdes tidak valid.").optional(),
+  kopdesId: z.string().optional().nullable(),
 });
